0bb42779e3693343a19043e26deb5a5e47432cea
Sylvain Hellegouarch <sh@defuze.org>
2023-02-27 10:49:08 +0100

    fix dependencies
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ with io.open('requirements.txt') as f:
 
 setup_params = dict(
     name=name,
-    version=__version__,
+    version=get_version_from_package(),
     description=desc,
     long_description=long_desc,
     long_description_content_type='text/markdown',
